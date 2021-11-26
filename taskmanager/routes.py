6b071c8f6f58bd99a96f7fcb9305f13401a15e7b
Joanna Gorska <gorska919@gmail.com>
2021-11-26 12:06:24 +0000

    Add comments to routes.py file

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -20,8 +20,17 @@ def categories():
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
     if request.method == "POST":
+        # post method will create new variable category that is set to 
+        # new instance of Category model (the table header from the models.py)
+        # name attribute in the form needs to match database model
+
         category = Category(category_name=request.form.get("category_name"))
         db.session.add(category)
         db.session.commit()
+        # after the data is submitted to the database, we are redirecting
+        # the user back to categories.html page
         return redirect(url_for("categories"))
+
+    # default method is get - so this will return "add_category.html" unless the method is POST
+    # it is good practice to add error handling and defence programing here
     return render_template("add_category.html")
