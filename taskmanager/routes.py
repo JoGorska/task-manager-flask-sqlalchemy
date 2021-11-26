44c9227464324f0d9b1a983e717bcf82fbd6be10
Joanna Gorska <gorska919@gmail.com>
2021-11-26 11:42:23 +0000

    Add categories.html and add_category.html and updated routes.py to render those files

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -6,3 +6,22 @@ from taskmanager.models import Category, Task
 @app.route("/")
 def home():
     return render_template("tasks.html")
+
+# navbar reffers to the name of the junction not the @app.route("/categories")
+
+@app.route("/categories")
+def categories():
+    return render_template("categories.html")
+
+# need to include methods here
+# get is to get the page
+# post is to post data to database
+
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+    if request.method == "POST":
+        category = Category(category_name=request.form.get("category_name"))
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("add_category.html")
