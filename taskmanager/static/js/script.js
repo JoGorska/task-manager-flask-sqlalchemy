b2bd5b44cc6be68d7bef0ffd40ada35d084a55df
Joanna Gorska <gorska919@gmail.com>
2021-11-28 09:41:54 +0000

    Add input fields to the form in add_task.html

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -12,12 +12,22 @@ $(document).ready(function(){
     $('.modal').modal();
   });
 
-  // datepicker initialization
-  $(document).ready(function(){
-    $('.datepicker').datepicker();
-  });
+  // // datepicker initialization
+  // $(document).ready(function(){
+  //   $('.datepicker').datepicker();
+  // });
 
   // select initialization
   $(document).ready(function(){
     $('select').formSelect();
   });
+
+  document.addEventListener("DOMContentLoaded", function() {
+
+      // datepicker initialization
+      let datepicker = document.querySelectorAll(".datepicker");
+      M.Datepicker.init(datepicker, {
+          format: "dd mmmm, yyyy",
+          i18n: {done: "Select"}
+      });
+    });
