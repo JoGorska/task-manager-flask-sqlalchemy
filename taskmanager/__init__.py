75ba3ed51298ab426182cd773991efc9764d01df
Joanna Gorska <gorska919@gmail.com>
2021-11-29 11:01:27 +0000

    Add heroku's database to init.py file

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -11,7 +11,14 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")  # local
+else:
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri  # heroku
+
 
 # this line was added due to error:
 # SQLALCHEMY_TRACK_MODIFICATIONS adds significant overhead and will be disabled by default in the future.  Set it to True to suppress this warning.
